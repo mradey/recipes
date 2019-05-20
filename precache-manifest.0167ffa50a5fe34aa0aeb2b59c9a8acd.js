29cbffecc69f36c373938cf3473d97046c85f936
Michael James Radey <radeymichael@gmail.com>
2019-05-20 15:12:20 -0400

    Updates

---
+++ precache-manifest.0167ffa50a5fe34aa0aeb2b59c9a8acd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ac4d03bf9239157602d6fa259f8a00",
+    "url": "/recipes/index.html"
+  },
+  {
+    "revision": "638baf25606b6964eaca",
+    "url": "/recipes/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/recipes/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "638baf25606b6964eaca",
+    "url": "/recipes/static/js/main.9cc0fa00.chunk.js"
+  },
+  {
+    "revision": "c96e90947479d05374fe",
+    "url": "/recipes/static/js/runtime~main.19e64e71.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/recipes/static/media/logo.5d5d9eef.svg"
+  }
+]);
